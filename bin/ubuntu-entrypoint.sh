5c7a99dad6468486edb326b9e2b75418edfe2ac4
Anthonius Munthi <me@itstoni.com>
2020-02-25 00:15:41 +0800

    test

---
+++ bin/ubuntu-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$USE_MIRROR" = 'yes' ]; then
+  MIRROR=`wget -qO - http://mirrors.ubuntu.com/mirrors.txt | head -1 | sed 's#http://##' | sed 's#/ubuntu/##g' `;
+  sed -i "s/archive.ubuntu.com/${MIRROR}/g" /etc/apt/sources.list;
+  sed -i "s/security.ubuntu.com/${MIRROR}/g" /etc/apt/sources.list;
+fi
+
+exec /lib/systemd/systemd
