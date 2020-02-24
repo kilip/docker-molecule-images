484a9d10c8e9f7306a39d1003ce75ef7240ea23b
Anthonius Munthi <me@itstoni.com>
2020-02-24 23:23:29 +0800

    improved project layout

---
+++ test/molecule/default/tests/test_python.py
@@ -0,0 +1,18 @@
+"""Role testing files using testinfra."""
+
+import re
+
+def test_python_version(host):
+    """Assert that python installed"""
+    py    = host.run('python --version')
+    pip   = host.run('pip --version')
+    version = py.stdout
+
+    if version == "":
+      version = py.stderr
+
+    assert py.rc == 0
+    assert version.find('Python') == 0
+
+    assert pip.rc == 0
+    assert pip.stdout.find('pip') == 0
