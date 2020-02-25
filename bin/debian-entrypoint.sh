5a260ffe8773a5882ec9d8be37b71fe1a6dec0ba
Anthonius Munthi <me@itstoni.com>
2020-02-25 13:43:16 +0800

    removed mirror feature from debian

--- bin/debian-entrypoint.sh
+++ bin/debian-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ "$USE_MIRROR" = 'yes' ]; then
-  netselect-apt -c ${COUNTRY} -t 15 -a amd64 -n testing;
-  mv /etc/apt/sources.list /etc/apt/sources.list_backup;
-  mv sources.list /etc/apt/sources.list;
-fi
+#if [ "$USE_MIRROR" = 'yes' ]; then
+#  netselect-apt -c ${COUNTRY} -t 15 -a amd64 -n testing;
+#  mv /etc/apt/sources.list /etc/apt/sources.list_backup;
+#  mv sources.list /etc/apt/sources.list;
+#fi
 
 exec /lib/systemd/systemd
